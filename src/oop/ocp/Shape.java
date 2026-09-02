aee7476e9c7be2fa93a9deaa52acc9079546c574
Ei Thandar Bo <eithandarbo8@gmail.com>
2026-09-02 09:36:38 +0700

    Complete Lab09_1

--- src/oop/ocp/Shape.java
+++ src/oop/ocp/Shape.java
@@ -1,5 +1,6 @@
 package oop.ocp;
 
 public abstract class Shape {// Abstract base class for all geometric shapes
-    public abstract double getArea();// Abstract method to calculate the area; must be implemented by concrete subclasses
+    public abstract double getArea();// Abstract method to calculate the area;
+                                    // must be implemented by concrete subclasses
 }
